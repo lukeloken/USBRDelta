7efb1dcf350a9960769a1225d8c18f7180f91ad2
Luke <lukeloken@gmail.com>
2018-08-24 15:48:13 -0700

    Completed code to loop through all vertical profile excel files and create a machine readible version. Outputs are a clean csv with consistent column names and a single .png file with that days profiles. Outputs are located in the dropbox folder, while inputs are in the google drive folder.
    Code includes several steps to clean up files as the file structure is not always consisent over the 5 year sampling collection. Some of the data contain down and up casts, and I excluded profiles from later in the day. Some data contained multiple profiles for the same site and additional sites, but for consistency I only extracted the first profile at each site.

--- R/CompileVerticalProfiles.R
+++ R/CompileVerticalProfiles.R
@@ -24,6 +24,11 @@ YSIfilenames<-list.files(paste0(google_dir, "/Data/DepthProfiles"))
 
 #Exclude non-excel files
 YSIfilenames<-YSIfilenames[grep('.xls', YSIfilenames)]
+YSIfilenames<-YSIfilenames[-grep('may 27 2015 report.xlsx', YSIfilenames)]
+YSIfilenames<-YSIfilenames[-grep('Channel Data Depth Profiles Oct 21 2014 report', YSIfilenames)]
+YSIfilenames<-YSIfilenames[-grep('Channel Data Depth Profiles Oct 23 2014 report', YSIfilenames)]
+YSIfilenames<-YSIfilenames[-grep('Sac Ship Channel Surface Summary', YSIfilenames)]
+
 
 #If there is an open excel sheet exclude it from the loop of filenames
 if (length(grep('~', YSIfilenames))>0){
@@ -31,59 +36,59 @@ if (length(grep('~', YSIfilenames))>0){
 }
 
 # Loop through filenames and read in data
-i=16
-# for (i in 1:length(YSIfilenames)){
-for (i in 1:16){
+i=9
+for (i in 1:length(YSIfilenames)){
 YSI_list_i<-read_excel_allsheets(paste0(google_dir, "/Data/DepthProfiles/", YSIfilenames[i]))
 
-# Omit extra sheets
-# These are the names of excel sheets that identify the profiles of interest
-# Ocassionally data contain additional sheets for other processing/plotting
+#Order each sheet by depth
+depth_name<-names(YSI_list_i[[1]])[grep('Depth', names(YSI_list_i[[1]]))]
+
 stations<-c('16', '34','44', 'Pro', '56','62', '64','66', '70','74', '76','84', 'WSP')
 greps<-c('16', '34','44', 'Pro', '56','62', '64','66', '70','74', '76','84', 'W')
-sheets<-as.numeric(sapply(greps, grep, x=names(YSI_list_i)))
-YSI_list_i2<-YSI_list_i[sheets[is.finite(sheets)]]
 
-names(YSI_list_i2)[[grep('W', names(YSI_list_i2))]]<-'WSP'
+if (length(YSI_list_i)==1){
+  #This is only the case for one date (First date June 2012)
+  YSI_list_i_noNA<-lapply(YSI_list_i, function(l) l[!is.na(l[,depth_name]),])
+  YSI_df_i<-ldply(YSI_list_i_noNA, data.frame)
+  YSI_df_i<-YSI_df_i[,-1]
+  YSI_df_i$'.id'<-YSI_df_i$Station
+  YSI_df_i<-YSI_df_i[which(YSI_df_i$Station %in% stations),]
 
-# Need to exclude multiple profiles added to the same column
-# See April 28, 2016 as an example
-# Time window between rows should be less than 5 minutes
+} else if  (length(YSI_list_i)>1){
 
-# df<-YSI_list_i2[[4]]
-threshold=300 #threshold between observations to note the next profile
-FirstProfile<-function(df){
-  diffs<-seconds(diff(df$Time))
-  row<-which(diffs>300)[1]
-  if(is.na(row)){
-    first_df<- df
-  } else {
-    first_df<-df[1:row,]
+  # Omit extra sheets
+  # These are the names of excel sheets that identify the profiles of interest
+  # Ocassionally data contain additional sheets for other processing/plotting
+  sheets<-as.numeric(sapply(greps, grep, x=names(YSI_list_i)))
+  YSI_list_i2<-YSI_list_i[sheets[is.finite(sheets)]]
+  
+  names(YSI_list_i2)[[grep('W', names(YSI_list_i2))]]<-'WSP'
+  
+  
+  # Need to exclude multiple profiles added to the same column
+  # See April 28, 2016 as an example
+  # Time window between rows should be less than 5 minutes
+  # df<-YSI_list_i2[[4]]
+  threshold=300 #threshold between observations to note the next profile
+  FirstProfile<-function(df){
+    diffs<-seconds(diff(df$Time))
+    row<-which(diffs>300)[1]
+    if(is.na(row)){
+      first_df<- df
+    } else {
+      first_df<-df[1:row,]
+    }
+    return(first_df)
   }
-  return(first_df)
-}
+  
+  YSI_list_i3<-lapply(YSI_list_i2, function(l) FirstProfile(l))
+  
+  
+  YSI_list_i_ordered<-lapply(YSI_list_i3, function(l) l[order(l[,depth_name]),])
+  YSI_list_i_noNA<-lapply(YSI_list_i_ordered, function(l) l[!is.na(l[,depth_name]),])
+  
+  YSI_df_i<-ldply(YSI_list_i_noNA, data.frame)
 
-YSI_list_i3<-lapply(YSI_list_i2, function(l) FirstProfile(l))
-
-#Order each sheet by depth
-YSI_list_i_ordered<-lapply(YSI_list_i3, function(l) l[order(l$'Depth'),])
-YSI_list_i_noNA<-lapply(YSI_list_i_ordered, function(l) l[!is.na(l$'Depth'),])
-
-YSI_df_i<-ldply(YSI_list_i_noNA, data.frame)
-
-column_names<-names(YSI_df_i)
-
-YSI_df_i$Date<-as.Date(YSI_df_i$Date)
-
-if ('Time.HH.MM.SS' %in% column_names){
-YSI_df_i$Time<-strftime(force_tz(YSI_df_i$Time.HH.MM.SS, "America/Los_Angeles"), format="%H:%M:%S")
-} else {
-  YSI_df_i$Time<-strftime(force_tz(YSI_df_i[,2], "America/Los_Angeles"), format="%H:%M:%S")
-}
-YSI_df_i$DateTime.PT<-ymd_hms(paste(YSI_df_i$Date, YSI_df_i$Time), tz='America/Los_Angeles')
-
-#Used for final name
-Date<-median(as.Date(YSI_df_i$Date), na.rm=T)
 
 #Change station names
 site<-stations[2]
@@ -91,9 +96,47 @@ for (site in stations){
   YSI_df_i$'.id'[grep(site, YSI_df_i$'.id')]<-site
 }
 
+}
+
+
+
+
+#Identify column names of compiled data.frame and make corrections if they are in the wrong format (e.g., meters vs ft)
+column_names<-names(YSI_df_i)
+Date_name<-column_names[grep('Date',column_names)]
+Time_name<-column_names[grep('Time',column_names)]
+
+
+if (length(Date_name)==1){
+YSI_df_i$Date<-as.Date(YSI_df_i[,Date_name])
+} else if (length(Date_name)==0){
+  warning ('No column named Date') 
+} else if  (length(Date_name)==2){
+  YSI_df_i$Date<-YSI_df_i[,Date_name[1]]
+  YSI_df_i[is.na(YSI_df_i$Date), 'Date']<-YSI_df_i[is.na(YSI_df_i$Date), Date_name[2]]
+}
+
+YSI_df_i$Time<-strftime(force_tz(YSI_df_i[,Time_name], "America/Los_Angeles"), format="%H:%M:%S")
+YSI_df_i$DateTime.PT<-ymd_hms(paste(YSI_df_i$Date, YSI_df_i$Time), tz='America/Los_Angeles')
+
+#Used for final name
+Date<-median(as.Date(YSI_df_i$Date), na.rm=T)
+
+
 if ('Depth.meters' %in% column_names){
   YSI_df_i$Depth.feet <- YSI_df_i$Depth.meters*3.28084
 }
+
+if ('Temp.F' %in% column_names){
+  YSI_df_i$Temp.C <- (YSI_df_i$Temp.F-32)*5/9
+}
+
+if ('ODO..' %in% column_names){
+  YSI_df_i$ODOsat.. <- (YSI_df_i$'ODO..')
+}
+
+
+
 
 variables<-c("Station", "DateTime.PT", "Depth.feet", "Temp.C", "SpCond.uS", "pH", "Chl.ug.L", "Chl.RFU", "ODOsat..", "ODO.mg.L", "Turbid..NTU")
 
@@ -102,7 +145,13 @@ YSI_df_i$Station<-factor(YSI_df_i$'.id', stations)
 YSI_df_i<-YSI_df_i[which(YSI_df_i$Station %in% stations),]
 
 df_plot<-YSI_df_i[intersect(variables, names(YSI_df_i))]
+MissingVars<-setdiff(variables, names(YSI_df_i))
+if (length(MissingVars)>0){
+  warning (paste('Missing variables:', Date, MissingVars, collapse=', '))
+}
+
 df_plot<-df_plot[order(df_plot$DateTime.PT),]
+df_plot<-df_plot[order(df_plot$Depth.feet),]
 df_plot<-df_plot[order(df_plot$Station),]
 write.csv(df_plot, file=paste0(dropbox_dir, '/Data/YSIVerticalProfiles/VertialProfile_', Date, '.csv'), row.names=F)
 
