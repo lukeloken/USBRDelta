069c555c945cbd9639115c918e4dcab929e90bef
Luke <lukeloken@gmail.com>
2018-10-18 15:38:49 -0700

    Adding incubation data and EXO profiles

--- R/Workflow_NutrientExperiment.R
+++ R/Workflow_NutrientExperiment.R
@@ -24,10 +24,16 @@ dropbox_dir<-'C:/Dropbox/USBR Delta Project'
 #Where data come from
 google_dir<-'C:/GoogleDrive/DeltaNutrientExperiment'
 
+#Field data
+source('R/CleanFieldData_NutrientExperiment.R')
+
+#Incubation data
 source('R/IncubationMetabolismLightDark_Workflow.R')
 source('R/Plot_IncubationMetabolism_Timeseries.R')
 
+#Vertical profiles
 source('R/CompileVerticalProfiles_NutrientExperiment.R')
+source('R/CompileVerticalProfiles_NutrientExperiment_EXO.R')
 
 source('R/ExtractSurfaceYSI_NutrientExperiment.R')
 source('R/MergeSurfaceYSIChemistryMetabolism_NutrientExperiment.R')
