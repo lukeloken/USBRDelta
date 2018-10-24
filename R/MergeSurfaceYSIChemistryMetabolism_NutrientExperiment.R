0fbde0f4fa70f5033759c34bc56b82495aaedc0a
Luke <lukeloken@gmail.com>
2018-10-24 14:13:59 -0700

    added field notes and nutrient data

--- R/MergeSurfaceYSIChemistryMetabolism_NutrientExperiment.R
+++ R/MergeSurfaceYSIChemistryMetabolism_NutrientExperiment.R
@@ -21,13 +21,24 @@ dropbox_dir<-'C:/Dropbox/USBR Delta Project'
 #Where data come from
 google_dir<-'C:/GoogleDrive/DeltaNutrientExperiment'
 
-# Find all filenames in directory
-# These will be used to loop through all old data
+sitetable<-data.frame(site1=c('NL70', 'EC2','EC3','EC4','EC5','EC6','EC7','EC8','NL76'), site2=c( "SSCN01_NV70", "SSCN02", "SSCN03", "SSCN04", "SSCN05", "SSCN06", "SSCN07", "SSCN08", "SSCN09 NL76"), site4=1:9)
 
+#Field notes
+field_df<-read.csv(file=paste0(dropbox_dir, '/Data/NutrientExperiment/FieldData.csv'), sep=',', header=T, stringsAsFactors = F)
+field_df$Date<-as.Date(field_df$Date)
+field_df$Site<-sitetable$site1[match(field_df$Location, sitetable$site4)]
+
+
+#Water chemistry
+nutrient_df<-read.csv(file=paste0(dropbox_dir, '/Data/NutrientExperiment/NutrientData.csv'), sep=',', header=T, stringsAsFactors = F)
+nutrient_df$Date<-as.Date(nutrient_df$Date)
+nutrient_df<-nutrient_df[which(nutrient_df$Site %in% sitetable$site1),]
+nutrient_surface<-nutrient_df[grep('_S', nutrient_df$SampleLabel),]
+nutrient_surface<-select(nutrient_surface, -Turbidity, -EC, -PH, -Lab..) 
+
+#YSI surface data
 bigdf <- read.csv(file=paste0(dropbox_dir, '/Data/NutrientExperiment/SurfaceChemistry/YSISurface.csv'), stringsAsFactors = F)
 bigdf$Date<-as.Date(bigdf$Date, format='%Y-%m-%d')
-
-sitetable<-data.frame(site1=c('NL70', 'EC2','EC3','EC4','EC5','EC6','EC7','EC8','NL76'), site2=c( "SSCN01_NV70", "SSCN02", "SSCN03", "SSCN04", "SSCN05", "SSCN06", "SSCN07", "SSCN08", "SSCN09 NL76"))
 
 bigdf$Site<-sitetable$site1[match(bigdf$Station, sitetable$site2)]
 
@@ -86,7 +97,13 @@ incubation_df<-summary_df %>%
 merge1<-full_join(incubation_df, bigdf)
 merge2<-select(merge1, -Station, -Depth.feet,- DateTime.PT)
 
+#Field notes
+merge3<-full_join(merge2, field_df)
+
+#Nutrient data
+merge4<-full_join(merge3, nutrient_surface)
+
 
 #Merge water chemistry data
-merge_df<-merge2
+merge_df<-merge4
 write.csv(merge_df, file=paste0(dropbox_dir, '/Data/NutrientExperiment/SurfaceChemistry/YSIMetabolismSurface.csv'), row.names=F)
