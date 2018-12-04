8da61a2824c455d019e75b382a79eb31ba60de02
Luke <lukeloken@gmail.com>
2018-12-04 11:35:24 -0800

    for one station use the hydrodynamics oxygen data

--- R/DownloadUSGSBuoy.R
+++ R/DownloadUSGSBuoy.R
@@ -8,7 +8,7 @@ startDate <- "2018-09-20"
 endDate <- "2018-10-31"
 
 # Get USGS gauge data for all SSCN stations.
-siteNumbers<-c("11455095") # NL72
+siteNumbers<-c("11455335", "11455142", "11455136", "11455095", "11447605") # NL54, 62, 66, 72, WestSac
 
 siteINFO<-readNWISsite(siteNumbers)
 
@@ -17,4 +17,9 @@ RawData <- renameNWISColumns(RawData)
 
 attr(RawData$dateTime, "tzone") <- 'America/Los_Angeles'
 
-plot(RawData$dateTime, RawData$DO_Inst, type='l')
+RawData$site_no <- factor(RawData$site_no, siteNumbers)
+
+#For one station use the Hydro dataset
+RawData$DO_Inst[which(RawData$site_no=="11455142")]<-RawData$DWS.BOR...HYDRO.PROJECT._DO_Inst[which(RawData$site_no=="11455142")]
+
+# plot(RawData$dateTime, RawData$DO_Inst, type='l')
