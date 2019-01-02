1e2d41b38b0af95543efc35c93246796e2e32671
Luke <lukeloken@gmail.com>
2019-01-02 12:28:19 -0800

    fixed sensorQC, so it doesn't call p2

--- R/SensorCleanLongitudinal.R
+++ R/SensorCleanLongitudinal.R
@@ -58,57 +58,64 @@ sensorclean<-function(datatable, ruletable){
 
   library(devtools)
   library(dplyr)
-
   library(sensorQC)
   
 # Create emtpy datatable to fill with cleaned values. 
 # Keep only column 1 (times)
-cleanedframe<- datatable
+clean_df<- datatable
+data_df<- datatable
+time_col<-which(names(clean_df)=='DateTime')
 
-time_col<-which(names(cleanedframe)=='DateTime')
-
-cleanedframe[,-time_col]<-NA
+clean_df[,-time_col]<-NA
 
 #Clean all variables in datatable except datetime column
 col=2
 par(mar=c(3,3,1,1), mgp=c(2,1,0))
-for (col in which(names(cleanedframe)!='DateTime')){
+for (col in which(names(clean_df)!='DateTime')){
   
   # Create sensor object and fill 'w' slot with rollingMAD
-  data<-datatable[,c(time_col,col)]
-  rules<- unlist(ruletable[which(ruletable[,1]==names(data)[2]), -1:-4])
+  data_col<-data_df[,c(time_col,col)]
+  rules<- unlist(ruletable[which(ruletable[,1]==names(data_col)[2]), -1:-4])
   
-  if (length(rules)==0 | class(data[,2])!='numeric'){
-    cleanedframe[,col]<-data[,2]
- 
-  } else { 
-  sensored<-sensor(data)
-  MADwindow<- unlist(ruletable[which(ruletable[,1]==names(data)[2]), 2])
-  Medianwindow<- unlist(ruletable[which(ruletable[,1]==names(data)[2]), 3])
-  sensored$sensor$w<-rollingMAD(sensored$sensor$x, Medianwindow, MADwindow)
-  
-  PersistMax<-unlist(ruletable[which(ruletable[,1]==names(data)[2]), 4])
-  p<-persist(sensored$sensor$x)
-  p2<-c(NA,p[-length(p)])
-  
-  sensored$sensor$x[which(p2>PersistMax)]<-NA
-  
-  
-  # Set rules manually or from ruletable and clean sensor object
-  # rules<-as.character(c('w>3', 'is.na(x)'))
+  if (length(rules)>0 & class(data_col[,2]) %in% c('numeric', 'integer')){
 
-  cleanedframe[,col]<-clean(sensored, rules, replace=NA)$sensor[,2] 
+    sensored<-sensor(data_col)
+    MADwindow<- unlist(ruletable[which(ruletable[,1]==names(data_col)[2]), 2])
+    Medianwindow<- unlist(ruletable[which(ruletable[,1]==names(data_col)[2]), 3])
+    sensored$sensor$w<-rollingMAD(sensored$sensor$x, Medianwindow, MADwindow)
+    
+    PersistMax<-unlist(ruletable[which(ruletable[,1]==names(data)[2]), 4])
+    p<-persist(sensored$sensor$x)
+    p_plus<-c(NA,p[-length(p)])
+    
+    sensored$sensor$x[which(p_plus>PersistMax)]<-NA
+    
+    # Set rules manually or from ruletable and clean sensor object
+    # rules<-as.character(c('w>3', 'is.na(x)'))
+    
+    clean_df[,col]<-clean(sensored, rules, replace=NA)$sensor[,2] 
     
     # Plot flagged (red) and retained (black) observations 
-    flags<-data[,2]
-    flags[which(flags==cleanedframe[,col])]<-NA
-    plot(cleanedframe[,col], col="black", ylab=names(data)[2], xlab="time", ylim=range(data[,2], na.rm=TRUE))
-    points(flags, col="red", pch=16)
+    flags_plot<-data_col[,2]
+    flags_plot[which(flags_plot==clean_df[,col])]<-NA
+    plot(clean_df[,col], col="black", ylab=names(data_col)[2], xlab="time", ylim=range(data_col[,2], na.rm=TRUE))
+    points(flags_plot, col="red", pch=16)
+    
+    rm(data_col)
+    rm(sensored)
+    rm(flags_plot)
+    rm(p)
+    rm(p_plus)
+  } else { 
+    clean_df[,col]<-data_col[,2]
+    
   }
-  
+
 }
 
-return(cleanedframe)
+return(clean_df)
+rm(clean_df)
+rm(data_df)
 }
 
 # =====================================================
