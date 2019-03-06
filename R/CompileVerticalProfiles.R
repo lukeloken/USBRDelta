0ec512e8f67deca28785d8f2e3f3196201bc57de
Luke <lukeloken@gmail.com>
2019-03-06 14:43:09 -0800

    new file

--- R/CompileVerticalProfiles.R
+++ R/CompileVerticalProfiles.R
@@ -30,7 +30,7 @@ YSIfilenames<-YSIfilenames[-grep('Channel Data Depth Profiles Oct 23 2014 report
 YSIfilenames<-YSIfilenames[-grep('Sac Ship Channel Surface Summary', YSIfilenames)]
 
 #or enter a single file
-YSIfilenames<-c('Channel Data depth Profiles January 30 2019.xlsx')
+YSIfilenames<-c('Channel Data depth Profiles February 28 2019.xlsx')
 
 #If there is an open excel sheet exclude it from the loop of filenames
 if (length(grep('~', YSIfilenames))>0){
