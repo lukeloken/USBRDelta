3bec86ef5b49ac168f16778bc45165d7b54e2c69
Luke <lukeloken@gmail.com>
2019-03-29 17:34:11 -0700

    updated filenames for most recent data

--- R/MergeSurfaceYSIChemistry.R
+++ R/MergeSurfaceYSIChemistry.R
@@ -27,9 +27,15 @@ google_dir<-'C:/GoogleDrive/DeltaNutrientExperiment'
 bigdf <- read.csv(file=paste0(dropbox_dir, '/Data/SurfaceChemistry/YSISurface.csv'), stringsAsFactors = F)
 bigdf$Date<-as.Date(bigdf$Date, format='%Y-%m-%d')
 
+nutrientname<-"SSC Nutrients March 2019.xlsx"
+
+
+
+
 #Data file as two rows as a header with mixed units/labels
-df1<-read_excel(paste0(google_dir, '/Data/WaterQuality/SSC Nutrients Jan 2019_withChla.xlsx'), skip=1)
-df2<-read_excel(paste0(google_dir, '/Data/WaterQuality/SSC Nutrients Jan 2019_withChla.xlsx'), skip=0)
+df1<-read_excel(paste0(google_dir, '/Data/WaterQuality/', nutrientname, sep=''), skip=1)
+df2<-read_excel(paste0(google_dir, '/Data/WaterQuality/', nutrientname, sep=''), skip=0)
+
 
 names1A<-gsub("\\_.*","",names(df1))
 names1B<-gsub("X", '', names1A)
@@ -47,7 +53,7 @@ names(df_sub)[which(names(df_sub)=="location")]<-'Station'
 names(df_sub)[which(names(df_sub)=="pH")]<-'pH_WQ'
 
 
-df_sub_noNA<-df_sub[!is.na(df_sub$Date),]
+df_sub_noNA<-df_sub[!is.na(df_sub$Date) & !is.na(df_sub$`Lab #`),]
 
 unique(df_sub_noNA$Station)
 
