cee8989783571a54daf5ab89f6e65801e679d490
Luke <lukeloken@gmail.com>
2019-04-01 10:17:16 -0700

    Cleaned code to work with wrapper script.

--- R/AggregatePhytos.R
+++ R/AggregatePhytos.R
@@ -17,24 +17,26 @@ dropbox_dir<-'C:/Dropbox/USBR Delta Project'
 google_dir<-'C:/GoogleDrive/DeltaNutrientExperiment'
 
 
-Phyto_df<-read.csv(file=paste(dropbox_dir, 'Data', 'Phyto', 'PhytoCountsAll.csv', sep='/'), stringsAsFactors = F)
-Phyto_df$DATE<-as.Date(Phyto_df$DATE)
+Phyto_FullRecord <- readRDS(file=paste0(dropbox_dir, '/Data/Rdata/Phyto_FullRecord.rds'))
+
+#Need to update everything below
+
 
 # ############################
 # Since data are 'count' data, need to populate dataset with zeros for any genus not counted in a given sample
 # ############################
 
 #All phyto species recorded
-GenusList<-unique(Phyto_df[c('GENUS', 'DIVISION')])
+GenusList<-unique(Phyto_FullRecord[c('GENUS', 'DIVISION')])
 
 #Unique combinations of Date and Site
-StationDates<-expand(Phyto_df, nesting(STATIONclean, DATE))
+StationDates<-expand(Phyto_FullRecord, nesting(Station, Date))
 
 #Create empty data.frame with every sample site/date with every phyto genus
 StationDatesGenus<-data.frame(matrix(ncol=4, nrow=nrow(StationDates)*nrow(GenusList)))
-names(StationDatesGenus)<-c("STATIONclean", "DATE", "GENUS", "DIVISION")
-StationDatesGenus$STATIONclean<-rep(StationDates$STATIONclean, each=nrow(GenusList))
-StationDatesGenus$DATE<-rep(StationDates$DATE, each=nrow(GenusList))
+names(StationDatesGenus)<-c("Station", "Date", "GENUS", "DIVISION")
+StationDatesGenus$Station<-rep(StationDates$Station, each=nrow(GenusList))
+StationDatesGenus$Date<-rep(StationDates$Date, each=nrow(GenusList))
 
 StationDate<-1
 for (StationDate in 1:nrow(StationDates)){
@@ -43,7 +45,7 @@ for (StationDate in 1:nrow(StationDates)){
 }
 
 #Join empty data.frame with observations
-Phyto_CompleteList<-full_join(StationDatesGenus, Phyto_df)
+Phyto_CompleteList<-full_join(StationDatesGenus, Phyto_FullRecord)
 
 #Replace NAs with zeros (not observed)
 Phyto_CompleteList$TALLY[which(is.na(Phyto_CompleteList$SAMPLE))]<-0
@@ -51,37 +53,49 @@ Phyto_CompleteList$DENSITY[which(is.na(Phyto_CompleteList$SAMPLE))]<-0
 Phyto_CompleteList$TOTAL.BV[which(is.na(Phyto_CompleteList$SAMPLE))]<-0
 
 
+# Modify columns structure to summarize and improve plotting
+Phyto_CompleteList <- Phyto_CompleteList %>%
+  dplyr::select(-'SAMPLE') %>%
+  mutate(Month=month(Date), 
+         Station=factor(Station, c('16', '34','44', 'Pro', '56','62', '64','66', '70','74', '76','84', 'WSP')))
+
+
+#Save complete recrod with NAs
+write.csv(Phyto_CompleteList, file=paste(google_dir, 'DataOutputs', 'Phyto_CompleteList', sep='/'), row.names=F)
+saveRDS(Phyto_CompleteList , file=paste0(dropbox_dir, '/Data/Rdata/Phyto_CompleteList.rds'))
+
+
 # ##################################################
 # Summarize data by division then by calendar month
 # ##################################################
 
-# Modify columns structure to summarize and improve plotting
-Phyto_CompleteList$Month<-month(Phyto_CompleteList$DATE)
-
-Phyto_CompleteList$STATIONclean=factor(Phyto_CompleteList$STATIONclean, c('16', '34','44', 'Pro', '56','62', '64','66', '70','74', '76','84', 'WSP'))
-
-#Save complete phyto record
-write.table(Phyto_CompleteList, file=paste(dropbox_dir, 'Data', 'Phyto', 'PhytoAllGenus.csv', sep='/'), row.names=F, sep=',')
 
 Phyto_summary<- Phyto_CompleteList %>%
-  select(STATIONclean,DATE, DIVISION, TOTAL.BV, DENSITY, Month) %>%
-  group_by(STATIONclean,DATE, DIVISION, Month) %>%
+  dplyr::select(Station,Date, DIVISION, TOTAL.BV, DENSITY, Month) %>%
+  group_by(Station,Date, DIVISION, Month) %>%
   summarize(Total_BioVolume=sum(TOTAL.BV)/(10^9), Density=sum(DENSITY)) %>%
-  drop_na(STATIONclean)
+  drop_na(Station)
 
 Phyto_summary_select<- Phyto_summary %>%
-  filter(DIVISION %in% c("Bacillariophyta", "Chlorophyta", "Cryptophyta", "Chrysophyta", "Cyanobacteria") & STATIONclean != '64')
+  filter(DIVISION %in% c("Bacillariophyta", "Chlorophyta", "Cryptophyta", "Chrysophyta", "Cyanobacteria") & Station != '64')
 
+#Export long summary table for ggplotting
+write.csv(Phyto_summary_select, file=paste(google_dir, 'DataOutputs', 'PhytoSummaryDivisionLongTable.csv', sep='/'), row.names=F)
+saveRDS(Phyto_summary_select , file=paste0(dropbox_dir, '/Data/Rdata/Phyto_summary_select.rds'))
+
+# Make a wide table for merging with other datasets
 Phyto_summary_spread <- Phyto_summary_select %>%
-  select(STATIONclean, DATE, DIVISION, Total_BioVolume) %>% 
+  dplyr::select(Station, Date, DIVISION, Total_BioVolume) %>% 
   spread(key = DIVISION, value= Total_BioVolume)
 
 #Export summary by division table to be merged with other datasets (Nutrients, Zoops, etc.)
-write.table(Phyto_summary_spread, file=paste(dropbox_dir, 'Data', 'Phyto', 'PhytoSummaryDivision.csv', sep='/'), row.names=F, sep=',')
+write.csv(Phyto_summary_spread, file=paste(google_dir, 'DataOutputs', 'PhytoSummaryDivision.csv', sep='/'), row.names=F)
+saveRDS(Phyto_summary_spread , file=paste0(dropbox_dir, '/Data/Rdata/Phyto_summary_spread.rds'))
+
 
 #Summarize by month to look at seasonal patterns
 Phyto_monthly <- Phyto_summary_select %>% 
-  group_by(STATIONclean, DIVISION, Month) %>%
+  group_by(Station, DIVISION, Month) %>%
   summarize(Mean_BioVolume=mean(Total_BioVolume, na.rm=T), Median_BioVolume=median(Total_BioVolume, na.rm=T), Mean_Density=mean(Density, na.rm=T))
 
 Phyto_monthly$Mean_BioVolume_log<-Phyto_monthly$Mean_BioVolume
@@ -92,292 +106,20 @@ Phyto_monthly$Median_BioVolume_log[which(Phyto_monthly$Median_BioVolume_log==0)]
 
 #Calculate total phyto biomass and summarize
 Phyto_total <- Phyto_summary %>% 
-  group_by(STATIONclean, DATE) %>%
+  group_by(Station, Date) %>%
   summarize(Total_BioVolume=sum(Total_BioVolume, na.rm=T), Total_Density=sum(Density, na.rm=T), Month=median(Month)) %>%
-  filter(STATIONclean != '64')
+  filter(Station != '64')
 
 Phyto_total_monthly <-Phyto_total  %>% 
-  group_by(STATIONclean, Month) %>%
+  group_by(Station, Month) %>%
   summarize(Mean_BioVolume=mean(Total_BioVolume, na.rm=T), Median_BioVolume=median(Total_BioVolume, na.rm=T), Mean_Density=mean(Total_Density, na.rm=T))
 
 
-write.table(Phyto_total, file=paste(dropbox_dir, 'Data', 'Phyto', 'PhytoTotals.csv', sep='/'), row.names=F, sep=',')
+#Export summary by division table to be merged with other datasets (Nutrients, Zoops, etc.)
+write.csv(Phyto_total, file=paste(google_dir, 'DataOutputs', 'PhytoTotalBiovolumes.csv', sep='/'), row.names=F)
+saveRDS(Phyto_total , file=paste0(dropbox_dir, '/Data/Rdata/Phyto_total.rds'))
 
 
-# ##############################
-# Phyto plots
-# ##############################
-
-BioVolExp<-expression(paste("Phytoplankton biovolume (1 x 10"^"9", " ", mu, "m"^"3", " L"^"-1", ")", sep=''))
-
-Median_BioVolExp<-expression(paste("Median phytoplankton biovolume (1 x 10"^"9", " ", mu, "m"^"3", " L"^"-1", ")", sep=''))
-
-#Boxplots by station
-colorset<-'Accent'
-colors<-brewer.pal(length(unique(Phyto_monthly$DIVISION)), colorset)
-colors<-colors[c(2,1,3,4,5)]
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom")
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/BoxplotGenusbyStation.png", sep=""), res=300, width=8,height=4, units="in")
-
-ggplot(Phyto_summary_select, aes(x=STATIONclean, y=Total_BioVolume, fill=DIVISION)) + 
-  labs(x='Station', y=BioVolExp) +
-  commonTheme_boxplot + 
-  # scale_y_log10(limits=c(1000, 100000000000)) +
-  scale_y_continuous(limits=c(0, 5)) +
-  guides(fill=guide_legend(title="")) + 
-  geom_boxplot(outlier.size=0.5, na.rm=T, outlier.shape=NA)
-
-dev.off()
-
-png(paste0(dropbox_dir, "/Figures/Phytos/DensityBoxplotGenusbyStation.png", sep=""), res=300, width=8,height=4, units="in")
-
-ggplot(Phyto_summary_select, aes(x=STATIONclean, y=Density, fill=DIVISION)) + 
-  labs(x='Station', y='Density') +
-  commonTheme_boxplot + 
-  scale_y_log10(limits=c(1, 100000000000)) +
-  # scale_y_continuous(limits=c(0, 100000000)) +
-  guides(fill=guide_legend(title="")) + 
-  geom_boxplot(outlier.size=0.5, na.rm=T, outlier.shape=NA)
-
-dev.off()
-
-# Boxplots by month
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom")
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/BoxplotGenusbyMonth.png", sep=""), res=300, width=8,height=4, units="in")
-
-
-ggplot(Phyto_summary_select, aes(x=as.factor(Month), y=Total_BioVolume, fill=DIVISION)) + 
-  labs(x='Month', y=BioVolExp) +
-  commonTheme_boxplot + 
-  # scale_y_log10() + 
-  scale_y_continuous(limits=c(0, 5)) +
-  geom_boxplot(outlier.size=0.5, na.rm=T, outlier.shape=NA) + 
-  guides(fill=guide_legend(title="")) 
-
-dev.off()
-
-
-#Boxplots by station by month by species
-
-#colors
-color.palette = colorRampPalette(c(viridis(6, begin=.2, end=.98), rev(magma(5, begin=.35, end=.98))), bias=1)
-colors<-color.palette(length(unique(Phyto_summary_select$Month)))
-shapes<-rep(21:25, 5)
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom")
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/BoxplotGenusbyMonthbyStation.png", sep=""), res=300, width=12,height=12, units="in")
-
-ggplot(Phyto_summary_select, aes(x= STATIONclean, y=Total_BioVolume, color=as.factor(Month))) + 
-  labs(x='Station', y=BioVolExp) +
-  commonTheme_boxplot + 
-  # scale_y_continuous(limits=c(0, 50)) +
-  scale_y_log10(limits=c(0.00001, 50)) +
-  geom_boxplot(outlier.size=0.5, na.rm=T, size=1, outlier.shape=NA) + 
-  # geom_point(na.rm=T) + 
-  facet_grid( DIVISION ~ .) + 
-  guides(color=guide_legend(title="Month"))
-
-dev.off()
-
-
-
-#Boxplots by month by station by species
-
-#colors
-color.palette = colorRampPalette(c(viridis(6, begin=.2, end=.98), rev(magma(5, begin=.35, end=.98))), bias=1)
-colors<-color.palette(length(unique(Phyto_summary_select$STATIONclean)))
-shapes<-rep(21:25, 5)
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom")
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/BoxplotGenusbyStationbyMonth.png", sep=""), res=300, width=12,height=12, units="in")
-
-ggplot(Phyto_summary_select, aes(x= as.factor(Month) , y=Total_BioVolume, color=STATIONclean)) + 
-  labs(x='Month', y=BioVolExp) +
-  commonTheme_boxplot + 
-  scale_y_log10(limits=c(.00001, 50)) + 
-  # scale_y_continuous(limits=c(0, 50)) +
-  geom_boxplot(outlier.size=0.5, na.rm=T, size=1, outlier.shape=NA) + 
-  # geom_point(na.rm=T) + 
-  facet_grid( DIVISION ~ .) + 
-  guides(color=guide_legend(title="Station"))
-
-dev.off()
-
-
-
-
-#Boxplots by month by station by species geom_area
-
-#colors
-colorset<-'Dark2'
-colors<-brewer.pal(length(unique(Phyto_summary_select$DIVISION)), colorset)
-
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom")
-)
-
-# png(paste0(dropbox_dir, "/Figures/Phytos/BoxplotGenusbyStationbyMonth.png", sep=""), res=300, width=12,height=12, units="in")
-
-png(paste0(dropbox_dir, "/Figures/Phytos/StatckedPlotDivisionbyStationbyDate.png", sep=""), res=300, width=6,height=12, units="in")
-
-
-ggplot(Phyto_summary_select, aes(x= DATE , y=Total_BioVolume, fill=DIVISION)) + 
-  labs(x='Month', y=BioVolExp) +
-  commonTheme_boxplot + 
-  # scale_y_log10(limits=c(10000, 10000000000)) + 
-  geom_area(position='stack') + 
-  # geom_path()
-  # geom_boxplot(outlier.size=0.5, na.rm=T, size=1, outlier.shape=NA) + 
-  # geom_point(na.rm=T) + 
-  facet_grid( STATIONclean ~ .)
-
-dev.off()
-
-
-# Stacked Area by month
-#colors
-colorset<-'Accent'
-colors<-brewer.pal(length(unique(Phyto_monthly$DIVISION)), colorset)
-colors<-colors[c(2,1,3,4,5)]
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom", 
-  panel.grid.major = element_blank(),
-  panel.grid.minor = element_blank())
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/StatckedPlotDivisionbyStationbyMonth.png", sep=""), res=300, width=6,height=12, units="in")
-
-ggplot(Phyto_monthly, aes(x= Month , y=Median_BioVolume, fill=DIVISION)) + 
-  labs(x='Month', y=Median_BioVolExp) +
-  commonTheme_boxplot + 
-  scale_y_continuous(limits=c(0, 7)) +
-  geom_area(position='stack') + 
-  # geom_path()
-  # geom_boxplot(outlier.size=0.5, na.rm=T, size=1, outlier.shape=NA) + 
-  # geom_point(na.rm=T) + 
-  scale_x_continuous(breaks=1:12) + 
-  guides(fill=guide_legend(title="")) + 
-  facet_grid( STATIONclean ~ .)
-
-dev.off()
-
-
-
-# lines by month/station/division
-#colors
-colorset<-'Accent'
-colors<-brewer.pal(length(unique(Phyto_monthly$DIVISION)), colorset)
-colors<-colors[c(2,1,3,4,5)]
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom", 
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/LinePlotDivisionbyStationbyMonth.png", sep=""), res=300, width=6,height=12, units="in")
-
-ggplot(Phyto_monthly, aes(x= Month , y=Median_BioVolume, color=DIVISION)) + 
-  labs(x='Month', y=Median_BioVolExp) +
-  commonTheme_boxplot + 
-  # scale_y_log10() +
-  # geom_jitter(size=3, width=0.1) + 
-  geom_path(size=2) + 
-  # geom_boxplot(outlier.size=0.5, na.rm=T, size=1, outlier.shape=NA) + 
-  # geom_point(na.rm=T) + 
-  scale_x_continuous(breaks=1:12) + 
-  facet_grid( STATIONclean ~ .) + 
-  guides(color=guide_legend(title=""))
-
-
-dev.off()
-
-
-
-# Total Phytoplankton biomass
-# lines by month/station/division
-#colors
-colorset<-'Accent'
-colors<-brewer.pal(length(unique(Phyto_monthly$DIVISION)), colorset)
-colors<-colors[c(2,1,3,4,5)]
-
-
-#Common theme for boxplot
-commonTheme_boxplot<-list(
-  scale_fill_manual(values = colors),
-  scale_colour_manual(values = colors),
-  theme_bw(),
-  theme(plot.title = element_text(hjust=0.5), legend.position="bottom", 
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-)
-
-png(paste0(dropbox_dir, "/Figures/Phytos/TotalPhyto_LinePlotDivisionbyStationbyMonth.png", sep=""), res=300, width=6,height=12, units="in")
-
-ggplot(Phyto_total_monthly, aes(x= Month , y=Median_BioVolume)) + 
-  labs(x='Month', y=Median_BioVolExp) +
-  commonTheme_boxplot + 
-  # scale_y_log10() +
-  # geom_jitter(size=3, width=0.1) + 
-  geom_path(size=2) + 
-  # geom_boxplot(outlier.size=0.5, na.rm=T, size=1, outlier.shape=NA) + 
-  # geom_point(na.rm=T) + 
-  scale_x_continuous(breaks=1:12) + 
-  facet_grid( STATIONclean ~ .) + 
-  guides(color=guide_legend(title=""))
-
-
-dev.off()
-
+rm(StationDate, GenusList, StationDates, StationDatesGenus)
 
 
