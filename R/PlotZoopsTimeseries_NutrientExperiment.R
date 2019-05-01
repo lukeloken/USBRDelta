56582a927033ea847af6066a32c077f6142ff651
Luke <lukeloken@gmail.com>
2019-05-01 13:26:09 -0700

    Added phytoplankton

--- R/PlotZoopsTimeseries_NutrientExperiment.R
+++ R/PlotZoopsTimeseries_NutrientExperiment.R
@@ -1,9 +1,8 @@
 
 
-
+Phyto_FullRecord <- readRDS(file=paste0(dropbox_dir, '/Data/Rdata/Phyto_FullRecord_NutExp1.rds'))
 
 Zoo_FullRecord <-readRDS(file=paste0(dropbox_dir, '/Data/Rdata/Zoo_FullRecord_NutExp1.rds'))
-
 
 pico_totals<- readRDS(file=paste0(dropbox_dir, '/Data/Rdata/Picos_FullRecord_NutExp1.rds'))
 
@@ -13,7 +12,29 @@ pico_totals_narrow <- pico_totals %>%
   gather(key='Division', value='TOTAL_BV_um3PerLiter', 3:4)
 
 
+#Phyto proessing
 
+Phyto_totals_genus<- Phyto_FullRecord %>%
+  dplyr::select(Date, Station, GENUS, TOTAL.BV) %>%
+  group_by(Date, Station, GENUS) %>%
+  dplyr::summarize(TOTAL.BV = sum(TOTAL.BV))
+
+head(Phyto_totals_genus)
+
+Phyto_totals_division<- Phyto_FullRecord %>%
+  dplyr::select(Date, Station, DIVISION, TOTAL.BV) %>%
+  group_by(Date, Station, DIVISION) %>%
+  dplyr::summarize(TOTAL.BV = sum(TOTAL.BV))
+
+head(Phyto_totals_division)
+
+Phyto_totals_all<- Phyto_FullRecord %>%
+  dplyr::select(Date, Station, TOTAL.BV) %>%
+  group_by(Date, Station) %>%
+  dplyr::summarize(TOTAL.BV = sum(TOTAL.BV))
+
+
+#Zooplankton processing
 
 Zoo_totals_genus<- Zoo_FullRecord %>%
   dplyr::select(Date, Station, genus, SpeciesBiomass_ugdwL) %>%
@@ -34,7 +55,7 @@ Zoo_totals_all<- Zoo_FullRecord %>%
   group_by(Date, Station) %>%
   dplyr::summarize(SpeciesBiomass_ugdwL = sum(SpeciesBiomass_ugdwL))
 
-
+#Plotting
 
 png(paste0(dropbox_dir, '/Figures/NutrientExperiment/Zoops/Zoops_AllGenus_TimeSeries.png'), units='in', width=8, height=6, res=400, bg='white')
 
@@ -76,6 +97,7 @@ dev.off()
 
 
 
+#Picoplankton
 
 png(paste0(dropbox_dir, '/Figures/NutrientExperiment/Picos/Picos_Totals_TimeSeries.png'), units='in', width=8, height=3, res=400, bg='white')
 
@@ -88,4 +110,52 @@ print(
 )
 
 dev.off()
+
+
+
+
+#Phytoplankton
+
+
+png(paste0(dropbox_dir, '/Figures/NutrientExperiment/Phytos/Phytos_Totals_Genus_TimeSeries.png'), units='in', width=12, height=6, res=400, bg='white')
+
+print(
+  ggplot(aes(x=Date, y=TOTAL.BV, group=Station, colour=Station), data=Phyto_totals_genus) + 
+    geom_point() +
+    geom_path() + 
+    facet_wrap(~GENUS) + 
+    theme_bw() + 
+    labs(y='Total Biovolume')
+)
+
+dev.off()
+
+
+
+png(paste0(dropbox_dir, '/Figures/NutrientExperiment/Phytos/Phytos_Totals_Division_TimeSeries.png'), units='in', width=6, height=4, res=400, bg='white')
+
+print(
+  ggplot(aes(x=Date, y=TOTAL.BV, group=Station, colour=Station), data=Phyto_totals_division) + 
+    geom_point() +
+    geom_path() + 
+    facet_wrap(~DIVISION) + 
+    theme_bw() + 
+    labs(y='Total Biovolume')
+)
+
+dev.off()
+
+
+png(paste0(dropbox_dir, '/Figures/NutrientExperiment/Phytos/Phytos_TotalBiomass_TimeSeries.png'), units='in', width=5, height=3, res=400, bg='white')
+
+print(
+  ggplot(aes(x=Date, y=TOTAL.BV, group=Station, colour=Station), data=Phyto_totals_all) + 
+    geom_point() +
+    geom_path() + 
+    theme_bw() + 
+    labs(y='Total Biovolume')
+)
+
+dev.off()
+
 
