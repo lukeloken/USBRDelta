cd7e0d799d52fbeb4937d2395b30a55d817b789e
Luke <lukeloken@gmail.com>
2019-10-01 12:31:24 -0700

    Script to process and merge a handful of water chemistry data tables. Nutrients, chla, TSS, etc are combined intoa  single data frame and saved.

---
+++ R/CleanNutrientData_NutrientExperiment2.R
@@ -0,0 +1,88 @@
+
+
+
+#Code to download meta and field sheet data
+
+library(readxl)
+library(dplyr)
+library(plyr)
+library(viridis)
+library(lubridate)
+library(ggplot2)
+library(gridExtra)
+library(stringr)
+
+# source('R/read_excel_allsheets.R')
+# source('R/g_legend.R')
+# 
+# # Project folder where outputs are stored
+# dropbox_dir<-'C:/Dropbox/USBR Delta Project'
+# 
+# #Where data come from
+# google_dir<-'C:/GoogleDrive/DeltaNutrientExperiment'
+
+
+nutrient_df<-read_excel(paste0(box_dir, "/Data/WaterChemistry/SSCN2_NutrientData.xlsx"), skip=1)
+
+nutrient_df_names<-names(read_excel(paste0(box_dir, "/Data/WaterChemistry/SSCN2_NutrientData.xlsx")))
+
+names(nutrient_df)<-nutrient_df_names
+rm(nutrient_df_names)
+
+#Reduce number of columns and calcuate new ones
+nutrient_df <- dplyr::select(nutrient_df, -starts_with('X__')) %>%
+  dplyr::select(-starts_with('Lab')) %>%
+  dplyr::select(-starts_with('AnalyteName')) %>%
+  drop_na(SampleCode) %>%
+  dplyr::mutate(
+    `DIN-ppm` = `NH4-ppm` + `NO3-ppm`, 
+    `DON-ppm` = `TDN-ppm` - `NH4-ppm` - `NO3-ppm`, 
+    `TPN-ppm` = `TN-ppm` - `TDN-ppm`, 
+    Date = as.Date(Date)
+  )
+
+# head(nutrient_df)
+
+
+#TSS data
+tss_df<-read_excel(paste0(box_dir, "/Data/WaterChemistry/SSCN2_TSSData.xlsx"), skip=1)
+
+tss_df_names<-names(read_excel(paste0(box_dir, "/Data/WaterChemistry/SSCN2_TSSData.xlsx")))
+
+names(tss_df)[1:7]<-tss_df_names[1:7]
+rm(tss_df_names)
+
+tss_df$SampleCode
+
+tss_df_sub<-tss_df[c("SampleCode", "Date", "Event", "Site", 'SiteCode', 'TSS', 'VSS')] %>%
+  mutate(Date=as.Date(Date))
+
+#ChlA
+
+#Oxygen18data
+# O18_batch1<-read.table(paste0(dropbox_dir,"/Data/NutrientExperiment/Oxygen18/LokenSadro_ExetData_decomposed_Bath01.txt"), sep='\t', skip=71, header=T, stringsAsFactors = F)
+# O18_batch2<-read.table(paste0(dropbox_dir,"/Data/NutrientExperiment/Oxygen18/LokenExetainers_Clean_decomposed_Batch02.txt"), sep='\t', skip=76, header=T, stringsAsFactors = F)
+# 
+# O18<-bind_rows(O18_batch1, O18_batch2)
+# 
+# O18$SampleLabel<-sub('_[ab]', '', O18$Group.1 )
+# 
+# O18avg<- O18 %>%
+#   dplyr::select(-Group.1, -flag.smallArea32) %>%
+#   group_by(SampleLabel) %>%
+#   dplyr::summarize_all(mean)
+# 
+
+
+nut_tss_df<-full_join(nutrient_df, tss_df_sub)
+
+# nut_tss_O18_df<-left_join(nut_tss_df, O18avg, by='SampleLabel')
+
+# nut_tss_O18_df$Site<-sitetable$site1[match(nut_tss_O18_df$Site, sitetable$site3)]
+
+full_chem_df <- nut_tss_df
+
+head(as.data.frame(full_chem_df))
+
+write.table(full_chem_df, file=paste0(google_dir, '/SSCN2_DataOutputs/AllWaterChemistry.csv'), row.names=F, sep=',')
+saveRDS(full_chem_df, file=paste0(dropbox_dir, '/Data/Rdata_SSCN2/AllWaterChemistry.rds'))
