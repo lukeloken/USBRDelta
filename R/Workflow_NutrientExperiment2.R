d23346e15558e5c43bff3da244a5a703fbde2ea7
Luke <lukeloken@gmail.com>
2019-10-29 11:21:54 -0700

    ad incubation metabolism calculation and plotting

--- R/Workflow_NutrientExperiment2.R
+++ R/Workflow_NutrientExperiment2.R
@@ -61,7 +61,7 @@ source('R/pCO2_computation_script_NutrientExperiment2.R')
 
 #Incubation data
 source('R/IncubationMetabolism_SSCN2_Workflow.R')
-# source('R/Plot_IncubationMetabolism_Timeseries_SSCN2.R')
+source('R/Plot_IncubationMetabolism_Timeseries_SSCN2.R')
 
 
 #Plot a bunch of stuff
