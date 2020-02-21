dd3b6fc23143f4fca5ce5dec6944d87d9c656b55
Luke Loken <lukeloken@gmail.com>
2020-02-21 17:10:32 -0600

    updating for onedrive

--- R/IncubationMetabolism_SSCN2_Workflow.R
+++ R/IncubationMetabolism_SSCN2_Workflow.R
@@ -22,13 +22,20 @@ source('R/CalculateIncubationMetabolism.R')
 # ##############################################################################################
 
 
+dir.create(file.path(onedrive_dir, "OutputData", "NutrientExperiment2", "IncubationMetabolism"), showWarnings = F)
+
+dir.create(file.path(onedrive_dir, "RData", "NutrientExperiment2", "IncubationMetabolism"), showWarnings = F)
+
+dir.create(file.path(onedrive_dir, "Figures", "NutrientExperiment2", "IncubationMetabolism"), showWarnings = F)
+
+
 #Plotting parameters
 jitterwidth=0.15
 colorset_incubation<-'Dark2'
 colors_incubation<-brewer.pal(3, colorset_incubation)[c(1,3,2)]
 
 
-directory_incubation<-paste(paste(box_dir,'Data/IncubationData/FormattedData', sep='/'))
+directory_incubation<-paste(file.path(onedrive_dir, 'RawData', 'NutrientExperiment2', 'IncubationData', 'FormattedData'))
 
 #Find the correct file and load it
 
@@ -62,12 +69,12 @@ for (file_nu in 1:length(files)){
     # write.csv(DOrate_mean_long_table, file=paste0(dropbox_dir, '/Data/Incubations/MetabolismCalculations/JarMetabolism_', Date, '.csv'), row.names=F)
     # 
     
-    write.csv(DOrate_mean_long_table, file=paste0(google_dir, '/SSCN2_DataOutputs/IncubationMetabolism/IncubationMetabolism_', Date_ymd, '.csv'), row.names=F)
-    saveRDS(DOrate_mean_long_table , file=paste0(dropbox_dir, '/Data/Rdata_SSCN2/IncubationMetabolism/IncubationMetabolism_', Date_ymd, '.rds'))
+    write.csv(DOrate_mean_long_table, file=file.path(onedrive_dir, 'OutputData', 'NutrientExperiment2', 'IncubationMetabolism', paste('IncubationMetabolism_', Date_ymd, '.csv')), row.names=F)
+    saveRDS(DOrate_mean_long_table , file=file.path(onedrive_dir, 'RData', 'NutrientExperiment2', 'IncubationMetabolism', paste('IncubationMetabolism_', Date_ymd, '.rds')))
 
     
     IncubationMetabolism_summary <- DOrate_mean_long_table %>%
-      select(-Jar, -Day) %>%
+      dplyr::select(-Jar, -Day) %>%
       group_by(Site, Metric, SampleDate, Treatment) %>%
       dplyr::summarize(MeanValue=mean(Value, na.rm=T), SDValue=sd(Value, na.rm=T))
     
@@ -148,7 +155,7 @@ for (file_nu in 1:length(files)){
     p2<-grid.arrange(grobs=plot_list, ncol=length(unique(uniquetable$Metric)), as.table=F)
     
     #Add legend to bottom of figure and save
-    png(paste0(dropbox_dir, '/Figures/NutrientExperiment2/Incubations/MetabolismTimeseries_', Date_ymd, '.png'), width=8, height=12, units='in', res=200)
+    png(file.path(onedrive_dir, 'Figures', 'NutrientExperiment2', 'IncubationMetabolism', paste0('MetabolismTimeseries_', Date_ymd, '.png')), width=8, height=12, units='in', res=200)
     
     grid.arrange(p2, mylegend, nrow=2,heights=c(10, length(unique(uniquetable$Site))/16))
     
@@ -165,7 +172,7 @@ for (file_nu in 1:length(files)){
     # #############################################
     
     
-    png(paste0(dropbox_dir, '/Figures/NutrientExperiment2/Incubations/MetabolismWithinSampleSD_', Date_ymd, '.png'), width=5, height=4, units='in', res=200)
+    png(file.path(onedrive_dir, 'Figures', 'NutrientExperiment2', 'IncubationMetabolism', paste0('MetabolismWithinSampleSD_', Date_ymd, '.png')), width=5, height=4, units='in', res=200)
     par(mar=c(2.5,3,0.5,0.5))
     par(mgp=c(3,0.5,0))
     xlabels<-paste0('T', 1:(ncol(DOsd)-1))
@@ -229,7 +236,7 @@ for (file_nu in 1:length(files)){
     
     
     #Add legend to bottom of figure and save
-    png(paste0(dropbox_dir, '/Figures/NutrientExperiment2/Incubations/MetabolismBoxplot_', Date_ymd, '.png'), width=8, height=3, units='in', res=200)
+    png(file.path(onedrive_dir, 'Figures', 'NutrientExperiment2', 'IncubationMetabolism', paste0('MetabolismBoxplot_', Date_ymd, '.png')), width=8, height=3, units='in', res=200)
     
     grid.arrange(p2_box, mylegend_box, nrow=2,heights=c(10, 1))
     
@@ -281,7 +288,7 @@ for (file_nu in 1:length(files)){
     
     
     #Add legend to bottom of figure and save
-    png(paste0(dropbox_dir, '/Figures/NutrientExperiment2/Incubations/LightTreatments_Boxplot_', Date_ymd, '.png'), width=8, height=3, units='in', res=200)
+    png(file.path(onedrive_dir, 'Figures', 'NutrientExperiment2', 'IncubationMetabolism', paste0('LightTreatments_Boxplot_', Date_ymd, '.png')), width=8, height=3, units='in', res=200)
     
     grid.arrange(p2_box, mylegend_box, nrow=2,heights=c(5, 1))
     
@@ -300,7 +307,7 @@ for (file_nu in 1:length(files)){
     plot_nu<-1
     
     IncubationMetabolism_spread<-IncubationMetabolism_summary %>%
-      select (Site, SampleDate, Metric, Treatment, MeanValue) %>%
+      dplyr::select (Site, SampleDate, Metric, Treatment, MeanValue) %>%
       spread(key=Metric, value=MeanValue) %>%
       mutate(ER=ER*(-1))
     
@@ -322,7 +329,7 @@ for (file_nu in 1:length(files)){
       theme(legend.background = element_rect(colour = "transparent", fill = "transparent"), legend.key = element_rect(colour = "transparent", fill = "transparent"))
       
     
-    png(paste0(dropbox_dir, '/Figures/NutrientExperiment2/Incubations/GPP_ER_Scatter_', Date_ymd, '.png'), width=5, height=5, units='in', res=200)
+    png(file.path(onedrive_dir, 'Figures', 'NutrientExperiment2', 'IncubationMetabolism', paste0('GPP_ER_Scatter_', Date_ymd, '.png')), width=5, height=5, units='in', res=200)
     
     print(GPP_ER_scatterplot)
     
